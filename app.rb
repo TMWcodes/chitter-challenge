b6ec85f3545c495fef8777f03e8b64c9e12c0f82
Tyrone Williams <tyrone@Tyrones-MacBook-Air.local>
2022-01-14 16:35:48 +0000

    set_up /peeps

---
+++ app.rb
@@ -0,0 +1,25 @@
+require 'sinatra/base'
+require 'sinatra/reloader'
+
+class BookmarkManager < Sinatra::Base
+    configure :development do
+      register Sinatra::Reloader
+    end
+  
+    get '/' do
+      'Hello World'
+    end
+
+    get '/peeps' do
+        peeps = [
+                  "peep1",
+                  "peep2",
+                  "peep3"
+                 ]
+      
+        peeps.join         
+        # erb :'peeps/index'
+      end
+  
+    run! if app_file == $0
+  end
---
+++ spec/features/Viewingpeeps_spec.rb
@@ -0,0 +1,11 @@
+
+
+feature 'Viewing peep' do
+    scenario 'A user can see peeps' do
+      visit('/peeps')
+  
+      expect(page).to have_content "peep1"
+    expect(page).to have_content "peep2"
+    expect(page).to have_content "peep3"
+  end
+end
